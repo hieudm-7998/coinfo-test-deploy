341ec29f2c24e7a717bd54ea3d0547dde6f883f9
Hieu Do <hieudm.7998@gmail.com>
2022-11-16 17:41:18 +0700

    netlify deploy config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  NETLIFY_USE_YARN: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
